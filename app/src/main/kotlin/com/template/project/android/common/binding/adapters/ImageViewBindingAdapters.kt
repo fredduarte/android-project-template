80aa5ea5f7cb878b3d1e278ad36e8f6ffa08ecc2
Frederico Simas Duarte <fredmdfk@gmail.com>
2021-01-24 21:14:38 +0100

    Created ImageView Binding Adapters file

---
+++ app/src/main/kotlin/com/template/project/android/common/binding/adapters/ImageViewBindingAdapters.kt
@@ -0,0 +1,14 @@
+package com.template.project.android.common.binding.adapters
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun imageUrl(view: AppCompatImageView, url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(url)
+            .into(view)
+    }
+}
