9b0d346d677ccd5aa5f1a543ee007d0434a6423f
Frederico Simas Duarte <fredmdfk@gmail.com>
2021-01-30 20:12:51 +0100

    Changed UserListItemUiModel to extend BindingUiModel

--- app/src/main/kotlin/com/template/project/android/mock/data/UserListItemUiModel.kt
+++ app/src/main/kotlin/com/template/project/android/mock/data/UserListItemUiModel.kt
@@ -1,8 +1,16 @@
 package com.template.project.android.mock.data
 
+import com.template.project.android.BR
+import com.template.project.android.R
+import com.template.project.android.common.recyclerview.BindingUiModel
+
 class UserListItemUiModel(
     val id: Int,
     val name: String,
     val age: Int,
     val profilePhoto: String? = null
-)
+) : BindingUiModel {
+
+    override val variableId: Int = BR.uiModel
+    override val layoutId: Int = R.layout.user_list_item
+}
