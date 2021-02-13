b68d388ab14d62d69be68d65c2d19048b5236875
Frederico Duarte <fredmdfk@gmail.com>
2021-02-13 17:59:05 +0100

    Created LayoutParameters class

---
+++ app/src/main/kotlin/com/template/project/android/common/LayoutParameters.kt
@@ -0,0 +1,8 @@
+package com.template.project.android.common
+
+import android.view.ViewGroup
+
+data class LayoutParameters(
+    val width: Int = ViewGroup.LayoutParams.MATCH_PARENT,
+    val height: Int = ViewGroup.LayoutParams.WRAP_CONTENT
+)
