27d7ba143f87b3ec7f0a17662d9730625394eb92
Frederico Duarte <fredmdfk@gmail.com>
2021-04-01 17:10:22 +0200

    Created initial project structure

---
+++ app/src/main/kotlin/com/template/project/android/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.template.project.android.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.template.project.android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
