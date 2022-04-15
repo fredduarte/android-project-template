dc6ad237dddbd48476464386fe17954d54eea860
Frederico Duarte <fredmdfk@gmail.com>
2022-04-15 17:16:28 +0200

    Changed UserListItemUiModel to be a BindingNestedUiModel

--- app/src/main/kotlin/com/template/project/android/mock/data/UserListItemUiModel.kt
+++ app/src/main/kotlin/com/template/project/android/mock/data/UserListItemUiModel.kt
@@ -2,15 +2,17 @@ package com.template.project.android.mock.data
 
 import com.template.project.android.BR
 import com.template.project.android.R
-import com.template.project.android.common.recyclerview.BindingUiModel
+import com.template.project.android.common.recyclerview.BindingNestedUiModel
+import com.template.project.android.common.recyclerview.RecyclerViewParameters
 
 class UserListItemUiModel(
     val id: Int,
     val name: String,
     val age: Int,
     val profilePhoto: String? = null
-) : BindingUiModel {
+) : BindingNestedUiModel {
 
     override val variableId: Int = BR.uiModel
     override val layoutId: Int = R.layout.user_list_item
+    override val recyclerParameters: RecyclerViewParameters? = null
 }
